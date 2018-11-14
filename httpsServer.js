f5a0918dd932ba2ae2c038aaf8128409b8b854ee
Dallas <dallas.a.huggins@gmail.com>
2018-11-14 17:48:44 -0500

    cleaning up

--- httpsServer.js
+++ httpsServer.js
@@ -15,17 +15,13 @@ const pw = encodeURIComponent(creds.mongodb_pw);
 const c = encodeURIComponent(creds.mongodb_cluster);
 let uri = `mongodb+srv://${un}:${pw}@${c}.mongodb.net`;
 // Files:
-//var routes = require('./routes/index.js');
 const RegistrantDB = require('./registrants').RegistrantDB;
 
 // Set up express
 const app = express();
-//app.set('port', 3001);
 app.set('view engine', 'html');
-//app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'build')));
 console.log("Dir name", __dirname);
-//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json(),cors(),helmet())
 const router = express.Router();
 var db;
@@ -39,9 +35,9 @@ const sslCredentials = {
 	cert: certificate,
 	ca: ca
 };
-
 const httpsServer = https.createServer(sslCredentials, app);
 
+// Open MongoDB connection:
 MongoClient.connect(uri, {useNewUrlParser: true}, function(err, database) {
     assert.equal(null, err);
     db = database;
@@ -73,43 +69,3 @@ MongoClient.connect(uri, {useNewUrlParser: true}, function(err, database) {
         }
     });
 });
-
-/*MongoClient.connect(uri, {useNewUrlParser: true}, function(err, db) {
-    "use strict";
-
-    assert.equal(null, err);
-    console.log("Successfully connected to MongoDB.");
-
-    var registrants = new RegistrantDB(db);
-
-    router.get("/", function (req, res) {
-        console.log(req, res);
-    });
-
-    router.post("/add", function (req, res) {
-        let body = req.body;
-        console.log("Body", body);
-        let object = {
-            name: body.name,
-            regCode: body.regCode,
-            status: body.status
-        }
-        try {
-            registrants.addItem(object, function(err, registrant) {
-                assert.equal(null, err);
-                console.log("Added", registrant);
-                res.status(200).send(registrant[0]);
-            });
-        } catch (e) {
-            console.log("Error:", e);
-            res.status(400).send(e.message);
-        }
-    });
-     // mounts the specified middleware function or functions at the specified path
-    //app.listen(3000); // binds and listens for connections on specified host and port
-    //db.close();
-    var server = app.listen(3000, function() {
-        var port = server.address().port;
-        console.log('Server listening on port %s.', port);
-    });
-});*/
