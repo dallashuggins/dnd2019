90e877a99a82c38097a2d3a392393cd42eb950a5
Dallas <dallas.a.huggins@gmail.com>
2018-12-01 13:10:25 -0500

    commented out

--- src/components/whatdoto.js
+++ src/components/whatdoto.js
@@ -24,51 +24,6 @@ function Plans (props) {
                     </p>
                 </div>
             </div>
-            {/*<button className="planAccordion accordionCity" onClick={()=>{
-                props.changeBool('accordionCity')
-            }}>Fun in the City</button>
-            <Collapse in={props.accordionCity}>
-                <div className="city">
-                    <h2 className="city">Boston</h2>
-                    <ul className="city">
-                        <li>Boston Public Gardens</li>
-                    </ul>
-                    <h2 className="city">Portland</h2>
-                    <ul className="city">
-                        <li>Portland Head Light</li>
-                    </ul>
-                </div>
-            </Collapse>
-            <button className="planAccordion accordionFall" onClick={()=>{
-                props.changeBool('accordionFall')
-            }}>Autumn time exploring</button>
-            <Collapse in={props.accordionFall}>
-                <div className="fall">
-                    <h2 className="fall">Hiking</h2>
-                    <ul className="fall">
-                        <li>Anywhere in the White Mountains</li>
-                    </ul>
-                    <h2 className="fall">Scenic drives</h2>
-                    <ul className="fall">
-                        <li>Kancamagus Highway</li>
-                    </ul>
-                </div>
-            </Collapse>
-            <button className="planAccordion accordionHalloween" onClick={()=>{
-                props.changeBool('accordionHalloween')
-            }}>HalloweEeEeEeeEeEeEen</button>
-            <Collapse in={props.accordionHalloween}>
-                <div className="halloween">
-                    <h2 className="halloween">Haunted Houses</h2>
-                    <ul className="halloween">
-                        <li>Haunted Overland</li>
-                    </ul>
-                    <h2 className="halloween">Corn mazes</h2>
-                    <ul className="halloween">
-                        <li>Emery Farms</li>
-                    </ul>
-                </div>
-            </Collapse>*/}
             <hr />
             <FallCarousel 
             />
